5b070970a4872e47958163558dd8e1cf120e3be2
yu_1kawa <fishirisjinxluk@gmail.com>
2026-09-09 20:13:39 +0500

    Task2

---
+++ Tyuiu.SmirnovaYV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.SmirnovaYV.Sprint0.Task2.V0.lib;
+namespace Tyuiu.SmirnovaYV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Юлия";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Юлия", res);
+        }
+    }
+}
---
+++ Tyuiu.SmirnovaYV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.SmirnovaYV.Sprint0.Task2.V0.lib;
+namespace Tyuiu.SmirnovaYV.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Юлия"));
+            Console.ReadKey();
+        }
+    }
+}

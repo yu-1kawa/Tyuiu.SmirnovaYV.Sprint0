5227814ac191a0a698295a2cce28143b5c98a197
user <user@DESKTOP-C7K5MN8>
2026-09-09 18:01:30 +0500

    Add project files.

---
+++ Tyuiu.SmirnovwYV.Sprint0.Task0.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.SmirnovwYV.Sprint0.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

894bd1194ecb4883f50e92ffcde9184b66dfbe3f
yu_1kawa <fishirisjinxluk@gmail.com>
2026-09-11 21:38:16 +0500

    Task7

---
+++ Tyuiu.SmirnovaYV.Sprint0.Task7.V0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.SmirnovaYV.Sprint0.Task7.V0.Lib;
+namespace Tyuiu.SmirnovaYV.Sprint0.Task7.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] nums1 = new int[] { 1, 1, 1, 1, 1 };
+            int[] nums2 = new int[] { 1, 1, 1, 1, 1 };
+            int[] nums3 = new int[] { 2, 2, 2, 2, 2 };
+
+            int[] res = DataService.AdditionArrays(nums1, nums2);
+            CollectionAssert.AreEqual(nums3, res);
+
+
+
+        }
+    }
+}
